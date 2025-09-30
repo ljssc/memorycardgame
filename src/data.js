18c95af698b4fea0623b0437137d4629a2a686a5
ljssc <igor.salnikov.00@bk.ru>
2025-09-30 11:10:13 +0500

    2 cards open

---
+++ src/data.js
@@ -0,0 +1,64 @@
+import img1 from './img/antimage_lg.jpg'
+import img2 from './img/dawnbreaker_lg.jpg'
+import img3 from './img/ogre_magi_lg.jpg'
+import img4 from './img/pudge_lg.jpg'
+import img5 from './img/skeleton_king_lg.jpg'
+import img6 from './img/spirit_breaker_lg.jpg'
+
+
+const images = [
+    {
+       "id":1,
+       "url":img1
+    },
+    {
+       "id":2,
+       "url":img2
+    },
+    {
+       "id":3,
+       "url":img3
+    },
+    {
+       "id":4,
+       "url":img4
+    },
+    {
+       "id":5,
+       "url":img5
+    },
+    {
+       "id":6,
+       "url":img6
+    },
+    {
+       "id":7,
+       "url":img1
+    },
+    {
+       "id":8,
+       "url":img2
+    },
+    {
+       "id":9,
+       "url":img3
+    },
+    {
+       "id":10,
+       "url":img4
+    },
+    {
+       "id":11,
+       "url":img5
+    },
+    {
+       "id":12,
+       "url":img6
+    }
+    
+
+    
+];
+
+
+export {images}
